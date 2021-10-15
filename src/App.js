57f576013d7c3e19285effb0df3008f9c644ded0
congtargets <884312741@qq.com>
2021-10-15 20:27:12 +0800

    测试

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import { thunk } from "redux-thunk";
+
+const initialState = {
+  usingRedux: true,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "save":
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+};
+const middleware = [thunk];
+console.log("middleware");
+console.log(middleware);
+console.log("process");
+console.log(process);
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createlogger());
+}
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
