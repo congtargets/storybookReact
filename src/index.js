63db5f70af57639074e6bd41c1506299c7fe2254
congfamin <844072720@qq.com>
2021-10-26 00:01:33 +0800

    d2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Clock from "./views/clock";
+import Clock from "./views/dOne/clock";
 import reportWebVitals from "./reportWebVitals";
 
 // import { createStore, applyMiddleware } from "redux";
