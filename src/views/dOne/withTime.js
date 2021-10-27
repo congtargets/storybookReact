32db4f96f39c664de78f0eb88688d5a1d0170e73
congfamin <844072720@qq.com>
2021-10-27 23:55:55 +0800

    1027d02

--- src/views/dOne/withTime.js
+++ src/views/dOne/withTime.js
@@ -17,7 +17,7 @@ export default function withTimer(WrappedComponent){
         render(){
             console.log('this.props');
             console.log(this.props);
-            return <wrappedComponent time={this.state.time}{...this.props}/>;
+            return <WrappedComponent time={this.state.time}{...this.props}/>;
         }
 
     };
